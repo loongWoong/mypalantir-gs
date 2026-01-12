2617ae6dae15ce977bf86b318631a669be94ae7f
Chun <caochun@gmail.com>
2026-01-12 15:18:14 +0800

    简化模型属性并配置data_source映射
    - 简化TollStation、TollLane、Gantry、Operator、Vehicle、Media模型，只保留数据表中存在的字段
    - 删除EntryTransaction中的snapshot_station_name（表中不存在）
    - 删除Passage中的ex_vehicle_type、pay_type、pay_card_type、l_date、status（表中不存在）
    - 为所有对象类型配置data_source和字段映射：
      - TollStation -> HIGHLINK.TOLL_STATIONS
      - TollLane -> HIGHLINK.TOLL_LANES
      - Gantry -> HIGHLINK.GANTRIES
      - Operator -> HIGHLINK.OPERATORS
      - Vehicle -> HIGHLINK.VEHICLES
      - Media -> HIGHLINK.MEDIA
      - EntryTransaction -> TBL_ENTRYWASTE
      - ExitTransaction -> TBL_EXITWASTE
      - GantryTransaction -> TBL_GANTRYWASTE
      - Passage -> TBL_PATH
      - PathDetail -> TBL_PATHDETAIL
    - 更新link_types中的表名配置（path_details -> TBL_PATHDETAIL）

---
+++ scripts/init_operators.sql
@@ -0,0 +1,26 @@
+-- 从 TBL_ENTRYWASTE, TBL_EXITWASTE 提取的操作员信息
+-- 操作员ID使用系统内部编号（OPERID）
+-- 班长信息作为操作员属性（monitor_id, monitor_name）
+
+INSERT INTO HIGHLINK.OPERATORS (oper_id, oper_name, monitor_id, monitor_name) VALUES ('999999999', 'ETC操作员', '999999999', 'ETC操作员');
+INSERT INTO HIGHLINK.OPERATORS (oper_id, oper_name, monitor_id, monitor_name) VALUES ('899999999', '收费员', NULL, NULL);
+INSERT INTO HIGHLINK.OPERATORS (oper_id, oper_name, monitor_id, monitor_name) VALUES ('040908049', '刘文青', '040908049', '刘文青');
+INSERT INTO HIGHLINK.OPERATORS (oper_id, oper_name, monitor_id, monitor_name) VALUES ('010106160', '王喜彦', '010106160', '王喜彦');
+INSERT INTO HIGHLINK.OPERATORS (oper_id, oper_name, monitor_id, monitor_name) VALUES ('120102059', '王丽霞', '120102059', '王丽霞');
+INSERT INTO HIGHLINK.OPERATORS (oper_id, oper_name, monitor_id, monitor_name) VALUES ('500601012', '王巧巧', '500601012', '王巧巧');
+INSERT INTO HIGHLINK.OPERATORS (oper_id, oper_name, monitor_id, monitor_name) VALUES ('190201204', '赵卫娜', '190201204', '赵卫娜');
+INSERT INTO HIGHLINK.OPERATORS (oper_id, oper_name, monitor_id, monitor_name) VALUES ('260104007', '谷啸晓', '260104007', '谷啸晓');
+INSERT INTO HIGHLINK.OPERATORS (oper_id, oper_name, monitor_id, monitor_name) VALUES ('160104050', '袁森', NULL, NULL);
+INSERT INTO HIGHLINK.OPERATORS (oper_id, oper_name, monitor_id, monitor_name) VALUES ('110103027', '尹香菊', '110103027', '尹香菊');
+INSERT INTO HIGHLINK.OPERATORS (oper_id, oper_name, monitor_id, monitor_name) VALUES ('040203022', '肖琴', '040203022', '肖琴');
+INSERT INTO HIGHLINK.OPERATORS (oper_id, oper_name, monitor_id, monitor_name) VALUES ('503004038', '苏俊蕊', '503004038', '苏俊蕊');
+INSERT INTO HIGHLINK.OPERATORS (oper_id, oper_name, monitor_id, monitor_name) VALUES ('660107013', '卫原', '660107013', '卫原');
+INSERT INTO HIGHLINK.OPERATORS (oper_id, oper_name, monitor_id, monitor_name) VALUES ('600201044', '李欣洋', '600201044', '李欣洋');
+INSERT INTO HIGHLINK.OPERATORS (oper_id, oper_name, monitor_id, monitor_name) VALUES ('050501026', '李彦华', '050501026', '李彦华');
+INSERT INTO HIGHLINK.OPERATORS (oper_id, oper_name, monitor_id, monitor_name) VALUES ('500603197', '宋旭东', '500603197', '宋旭东');
+INSERT INTO HIGHLINK.OPERATORS (oper_id, oper_name, monitor_id, monitor_name) VALUES ('220103009', '李姿颖', '220103009', '李姿颖');
+INSERT INTO HIGHLINK.OPERATORS (oper_id, oper_name, monitor_id, monitor_name) VALUES ('080204080', '姚友霞', '000000000', '何锦');
+INSERT INTO HIGHLINK.OPERATORS (oper_id, oper_name, monitor_id, monitor_name) VALUES ('020306138', '曹新勇', '020306138', '曹新勇');
+INSERT INTO HIGHLINK.OPERATORS (oper_id, oper_name, monitor_id, monitor_name) VALUES ('040105606', '蔡延青', '040105606', '蔡延青');
+INSERT INTO HIGHLINK.OPERATORS (oper_id, oper_name, monitor_id, monitor_name) VALUES ('060201115', '孙雯', '060201115', '孙雯');
+INSERT INTO HIGHLINK.OPERATORS (oper_id, oper_name, monitor_id, monitor_name) VALUES ('050303077', '杨翠', '050303077', '杨翠');
